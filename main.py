26faff7b6792e69e25c47d275a1135aecaa1c46a
Ellie Alavi <elhamalavi.f@gmail.com>
2025-07-06 19:58:07 -0400

    Added Recursive function

--- main.py
+++ main.py
@@ -1,36 +1,53 @@
-def consonant_count(characters):
-    pass
+# def consonant_count(characters):
+#     VOWELS = {'a','e','i','o','u'}
+#     result = 0
 
+#     for char in characters:
+#         if char.lower().isalpha() and char.lower() not in VOWELS:
+#                 result += 1
+    
+#     print(result)
+#     return result
+
+# ********************************
+# ********** Recursion ***********
+# ********************************
+def consonant_count(characters):
+    VOWELS = {'a','e','i','o','u'}
+
+    if len(characters) == 0:
+            return 0
+        
+    if characters[0].lower().isalpha() and characters[0].lower() not in VOWELS:
+        return consonant_count(characters[1:])+1
+    return consonant_count(characters[1:])
 
 ### Test Case #1
 
 characters = "abc de"
-
+print(consonant_count(characters))
 assert consonant_count(characters) == 3
 
-### Test Case #2
+# ### Test Case #2
 
-characters = "D E H$GJL iou"
+# characters = "D E H$GJL iou"
+# assert consonant_count(characters) == 5
 
-assert consonant_count(characters) == 5
+# ### Test Case #3
 
-### Test Case #3
+# characters = "593   8fDk%@  #@d889  2dAjf"
+# assert consonant_count(characters) == 7
 
-characters = "593   8fDk%@  #@d889  2dAjf"
+# ### Test Case #4
 
-assert consonant_count(characters) == 7
+# characters = "AaEeIiOoUu"
+# assert consonant_count(characters) == 0
 
-### Test Case #4
+# ### Test Case #5
 
-characters = "AaEeIiOoUu"
+# characters = ""
 
-assert consonant_count(characters) == 0
-
-### Test Case #5
-
-characters = ""
-
-assert consonant_count(characters) == 0
+# assert consonant_count(characters) == 0
 
 print("All tests passed!")
 print("Discuss time & space complexity if time remains.")
